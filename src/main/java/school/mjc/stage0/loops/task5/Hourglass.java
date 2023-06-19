946b500345d16da373170b2e3c7efa75d83b0766
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-06-20 01:15:25 +0200

    Done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,42 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int middle = height/2;
+        int temp = height;
+        int space = 0;
+        for(int i=0; i<height; i++){
+            if(height % 2 == 0)
+                if(i == middle +1){
+                    temp = temp + 2;
+                    space--;
+                }
+            for(int z=0; z < space; z++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<temp; j++){
+                System.out.print(8);
+            }
+            for(int z=0; z < space; z++){
+                System.out.print(" ");
+            }
+            if(height % 2 == 0) {
+                if(i<middle && temp > 2){
+                    temp = temp - 2;
+                    space++;}
+                else if(i>middle){
+                    temp = temp + 2;
+                    space--;}
+            }
+            else {
+                if(i>=middle){
+                    temp = temp + 2;
+                    space--;}
+                else{
+                    temp = temp - 2;
+                    space++;}
+            }
+            System.out.println();
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+        int temp = 1;
+        for(int i=0; i<cathetusLength; i++){
+            for(int j=0; j<temp; j++){
+                System.out.print(8);
+            }
+            temp++;
+            System.out.println();
+        }
 
     }
 }
